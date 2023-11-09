91a9b3d71ba5e57366555c109519045f93e68d2c
Gabriel Fu <hfu.gabriel@gmail.com>
2023-11-09 16:03:10 +0800

    add isCastlingValid

--- validation.go
+++ validation.go
@@ -53,5 +53,150 @@ func isSquareAttacked(sq Square, b *Board) bool {
 		}
 	}
 
-	return GetKingMoves(king.Squares()[0]).SquareIsSet(sq)
+	if king != 0 {
+		return GetKingMoves(king.Squares()[0]).SquareIsSet(sq)
+	}
+
+	return false
+}
+
+func isCastlingPathClear(from Square, to Square, b *Board) bool {
+	if from.Rank() != to.Rank() {
+		return false
+	}
+	if isSquareAttacked(from, b) {
+		return false
+	}
+	if from < to {
+		for i := from + 1; i <= to; i++ {
+			if isSquareAttacked(i, b) || b.allOccupied.SquareIsSet(i) {
+				return false
+			}
+		}
+	} else {
+		for i := from - 1; i >= to; i-- {
+			if isSquareAttacked(i, b) || b.allOccupied.SquareIsSet(i) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func isCastlingValid(move *Move, b *Board) bool {
+	if move.Piece() == WHITE_KING {
+		if b.turn != WHITE {
+			return false
+		}
+		if move.From() != E1 {
+			return false
+		}
+		if move.To() == C1 {
+			if move.Castle() != WHITE_QUEEN_SIDE {
+				return false
+			}
+			if !b.castlingRights.Has(WHITE_QUEEN_SIDE) {
+				return false
+			}
+		} else if move.To() == G1 {
+			if move.Castle() != WHITE_KING_SIDE {
+				return false
+			}
+			if !b.castlingRights.Has(WHITE_KING_SIDE) {
+				return false
+			}
+		} else {
+			return false
+		}
+	} else if move.Piece() == BLACK_KING {
+		if b.turn != BLACK {
+			return false
+		}
+		if move.From() != E8 {
+			return false
+		}
+		if move.To() == C8 {
+			if move.Castle() != BLACK_QUEEN_SIDE {
+				return false
+			}
+			if !b.castlingRights.Has(BLACK_QUEEN_SIDE) {
+				return false
+			}
+		} else if move.To() == G8 {
+			if move.Castle() != BLACK_KING_SIDE {
+				return false
+			}
+			if !b.castlingRights.Has(BLACK_KING_SIDE) {
+				return false
+			}
+		} else {
+			return false
+		}
+	}
+
+	if !isCastlingPathClear(move.From(), move.To(), b) {
+		return false
+	}
+	return true
+}
+
+// isMovable returns true if the from, to and piece are valid
+func isMovable(move *Move, b *Board) bool {
+	// validate not friendly occupied
+	allowedTos := ^b.whiteOccupied
+	if b.turn == BLACK {
+		allowedTos = ^b.blackOccupied
+	}
+	if !allowedTos.SquareIsSet(move.To()) {
+		return false
+	}
+
+	// validate piece is currently at "from"
+	p := b.GetPieceAtSquare(move.From())
+	if p != move.Piece() {
+		return false
+	}
+
+	// validate piece can move to "to"
+	switch p {
+	case WHITE_KNIGHT, BLACK_KNIGHT:
+		if !GetKnightMoves(move.From()).SquareIsSet(move.To()) {
+			return false
+		}
+	case WHITE_PAWN:
+		if !GetWhitePawnAttacks(move.From()).SquareIsSet(move.To()) &&
+			!GetWhitePawnMoves(move.From()).SquareIsSet(move.To()) {
+			return false
+		}
+	case BLACK_PAWN:
+		if !GetBlackPawnAttacks(move.From()).SquareIsSet(move.To()) &&
+			!GetBlackPawnMoves(move.From()).SquareIsSet(move.To()) {
+			return false
+		}
+	case WHITE_BISHOP, BLACK_BISHOP:
+		if !GetBishopMoves(move.From(), b.allOccupied).SquareIsSet(move.To()) {
+			return false
+		}
+	case WHITE_ROOK, BLACK_ROOK:
+		if !GetRookMoves(move.From(), b.allOccupied).SquareIsSet(move.To()) {
+			return false
+		}
+	case WHITE_QUEEN, BLACK_QUEEN:
+		if !GetQueenMoves(move.From(), b.allOccupied).SquareIsSet(move.To()) {
+			return false
+		}
+	case WHITE_KING, BLACK_KING:
+		if !GetKingMoves(move.From()).SquareIsSet(move.To()) {
+			return false
+		}
+	}
+	return true
+}
+
+func isMoveValid(move *Move, b *Board) bool {
+	if move.Castle() != 0 {
+		return isCastlingValid(move, b)
+	}
+	// TODO
+	return true
 }
--- validation_test.go
+++ validation_test.go
@@ -20,3 +20,60 @@ func TestIsSquareAttacked(t *testing.T) {
 		t.Errorf("E6 should be attacked by black pawn")
 	}
 }
+
+func TestIsCastlingValid(t *testing.T) {
+	b := NewEmptyBoard()
+	b.AddPieceToSquare(WHITE_ROOK, A1)
+	b.AddPieceToSquare(WHITE_ROOK, H1)
+	b.AddPieceToSquare(WHITE_KING, E1)
+
+	queenSideCastleMove := NewCastlingMove(E1, C1, WHITE_KING, WHITE_QUEEN_SIDE)
+	kingSideCastleMove := NewCastlingMove(E1, G1, WHITE_KING, WHITE_KING_SIDE)
+
+	if !isCastlingValid(queenSideCastleMove, b) {
+		t.Errorf("Castling whtie queen side should be valid")
+	}
+
+	if !isCastlingValid(kingSideCastleMove, b) {
+		t.Errorf("Castling whtie king side should be valid")
+	}
+
+	var cpy *Board
+	cpy = b.Copy()
+	cpy.Move(NewMove(E1, D1, WHITE_KING))
+	cpy.Move(NewMove(D1, E1, WHITE_KING))
+	if isCastlingValid(kingSideCastleMove, cpy) {
+		t.Errorf("Castling should be invalid after king has been moved")
+	}
+
+	cpy = b.Copy()
+	cpy.Move(NewMove(H1, G1, WHITE_ROOK))
+	cpy.Move(NewMove(G1, H1, WHITE_ROOK))
+	if isCastlingValid(kingSideCastleMove, cpy) {
+		t.Errorf("Castling should be invalid after rook has been moved")
+	}
+
+	cpy = b.Copy()
+	cpy.AddPieceToSquare(WHITE_KNIGHT, G1)
+	if isCastlingValid(kingSideCastleMove, cpy) {
+		t.Errorf("Castling should be invalid with knight in the way")
+	}
+
+	cpy = b.Copy()
+	cpy.AddPieceToSquare(BLACK_QUEEN, E8)
+	if isCastlingValid(kingSideCastleMove, cpy) {
+		t.Errorf("Castling should be invalid when in check")
+	}
+
+	cpy = b.Copy()
+	cpy.AddPieceToSquare(BLACK_QUEEN, F8)
+	if isCastlingValid(kingSideCastleMove, cpy) {
+		t.Errorf("Castling should be invalid through check")
+	}
+
+	cpy = b.Copy()
+	cpy.AddPieceToSquare(BLACK_QUEEN, G8)
+	if isCastlingValid(kingSideCastleMove, cpy) {
+		t.Errorf("Castling should be invalid when destination is in check")
+	}
+}
